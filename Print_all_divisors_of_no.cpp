32008ffab4e424a41c0f43c710df2043f908cd49
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-05-11 23:43:50 +0530

    Print all divisors

--- Print_all_divisors_of_no.cpp
+++ Print_all_divisors_of_no.cpp
@@ -6,11 +6,8 @@ using namespace std;
 
 vector<int> allDivisors(int N){
     vector<int> ans;
-    if(N == 0 ) 
-    return ans;
-
     for(int i = 1; i <= sqrt(N); i++){
-        if(i % N == 0){
+        if(N % i == 0){
             ans.push_back(i);
             if(N/i != i){
                 ans.push_back(N/i);
@@ -22,6 +19,13 @@ vector<int> allDivisors(int N){
 
 int main()
 {
-
+    int N = 36;
+    vector<int> ans;
+    ans = allDivisors(N);
+    cout<<"ANS :";
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
  return 0;
 }
